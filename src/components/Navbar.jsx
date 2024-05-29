950f31ce69fb10727ebd329df2170bea386bca4a
Grace Yin <graceyin0728@gmail.com>
2024-05-28 23:09:57 -0700

    again

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,11 +5,11 @@ import "./Navbar.css"
 export const Navbar = () => {
   return (
     <nav>
-        <Link to="/" className="title">Grace Yin's Family</Link>
+        <Link to="/History/" className="title">Grace Yin's Family</Link>
         <ul>
-            <li> <Link to="/about">尹奕</Link> </li>
-            <li> <Link to="/grandma">刘玳（奶奶）</Link></li>
-            <li> <Link to="/grandpa">尹树新（爷爷）</Link> </li>
+            <li> <Link to="/History/about">尹奕</Link> </li>
+            <li> <Link to="/History/grandma">刘玳（奶奶）</Link></li>
+            <li> <Link to="/History/grandpa">尹树新（爷爷）</Link> </li>
         </ul>
     </nav>
   )
